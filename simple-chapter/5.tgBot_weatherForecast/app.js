f4eb1547826c3eef10ea6b50167bc2bed9ac2df6
Vasyl Hryts <vasylhryts@knu.ua>
2023-04-11 19:06:44 +0300

    complete task

--- simple-chapter/5.tgBot_weatherForecast/app.js
+++ simple-chapter/5.tgBot_weatherForecast/app.js
@@ -14,30 +14,116 @@ app.listen(port, () => {
 })
 
 const token = `${process.env.TOKEN}`
-
 const bot = new TelegramBot(token, { polling: true })
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id
   const userInput = msg.text
 
-  try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${process.env.WEATHER_KEY}`
+  if (userInput === '/start') {
+    bot.sendMessage(
+      chatId,
+      'Hello! Welcome to the Weather Forecast Bot.\nPlease enter a city name.'
     )
-    const data = response.data
-    const weather = data.weather[0].description
-    const temperature = data.main.temp - 273.15
-    const city = data.name
-    const humidity = data.main.humidity
-    const pressure = data.main.pressure
-    const windSpeed = data.wind.speed
-    const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(
-      2
-    )}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`
+    return
+  }
 
-    bot.sendMessage(chatId, message)
-  } catch (error) {
-    bot.sendMessage(chatId, "City doesn't exist.")
+  if (userInput === '3 hours' || userInput === '6 hours') {
+  } else {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${process.env.WEATHER_KEY}`
+      )
+
+      const data = response.data
+      var inp_city = data.name
+
+      bot.sendMessage(chatId, `Select forecast interval for ${inp_city}:`, {
+        reply_markup: {
+          keyboard: [[{ text: '3 hours' }], [{ text: '6 hours' }]],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      })
+
+      bot.once('message', async (msg) => {
+        const chatId = msg.chat.id
+        const userInput = msg.text
+        var input_city = inp_city
+        if (userInput === '3 hours' || userInput === '6 hours') {
+          try {
+            const interval = userInput === '3 hours' ? 3 : 6
+            const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/forecast?q=${input_city}&appid=${process.env.WEATHER_KEY}`
+            )
+
+            const data = response.data
+            const city = data.city.name
+            const forecastList = data.list
+
+            const daysOfWeek = [
+              'Sunday',
+              'Monday',
+              'Tuesday',
+              'Wednesday',
+              'Thursday',
+              'Friday',
+              'Saturday',
+            ]
+
+            const months = [
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December',
+            ]
+
+            let message = `Weather forecast for ${city} every ${interval} hours:\n`
+            let dataTemp = ''
+
+            for (let i = 0; i < forecastList.length; i += interval / 3) {
+              const forecast = forecastList[i]
+
+              const dateTime = new Date(forecast.dt_txt)
+
+              const dayOfWeek = daysOfWeek[dateTime.getUTCDay()]
+              const dayOfMonth = dateTime.getUTCDate()
+              const month = months[dateTime.getUTCMonth()]
+              const hours = dateTime.getUTCHours()
+              const minutes = dateTime.getUTCMinutes()
+
+              const dataAll = `${dayOfWeek} ${dayOfMonth} ${month}`
+              if (dataAll != dataTemp) {
+                message += `\n\n${dataAll} \n`
+                dataTemp = dataAll
+              }
+
+              const formattedDateTime = `${hours.toString().padStart(2, '0')}:${minutes
+                .toString()
+                .padStart(2, '0')}`
+
+              const weatherDescription = forecast.weather[0].description
+              const temperature = (forecast.main.temp - 273.15).toFixed(2)
+              const feels_like = (forecast.main.feels_like - 273.15).toFixed(2)
+              message += `${formattedDateTime}, \t${temperature}°C \tFeels like: ${feels_like}°C, \t${weatherDescription}\n`
+            }
+
+            bot.sendMessage(chatId, message)
+          } catch (error) {
+            bot.sendMessage(chatId, 'Error getting weather forecast.')
+          }
+        }
+      })
+    } catch (error) {
+      bot.sendMessage(chatId, "City doesn't exist.")
+    }
   }
 })
